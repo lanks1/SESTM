b59d11571f31ac8cdb449cc0a00fb1b412d1e7c4
lanks1 <41757147+lanks1@users.noreply.github.com>
2018-08-13 10:02:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,19 @@ client.on('message',async message => {
   }
 });
 
+client.on("message", message => {
 
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

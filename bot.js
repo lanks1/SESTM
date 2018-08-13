337f25f5dfa8ec0ac0f5369f4cffb3e83cf3e1cd
lanks1 <41757147+lanks1@users.noreply.github.com>
2018-08-13 10:06:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,16 +113,7 @@ client.on('message', message => {
 }
 });
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`HD,~THE~BEST`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

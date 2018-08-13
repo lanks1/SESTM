2a446db00cac7f7514e5c4b98424f0756278455e
lanks1 <41757147+lanks1@users.noreply.github.com>
2018-08-13 09:41:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,8 +11,6 @@ client.on('message', message => {
       }
 });
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
